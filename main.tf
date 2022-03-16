f87eea9747818864ed5fa19f0b08192699ebd62b
Rajesh Pasam <rajeshpasam@197BLRMB30654.local>
2022-03-16 11:58:24 +0530

    removal of access key and scre key

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAYGNX3X3BEUE7X4U4"
-  secret_key = "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"
+  access_key = "xxx"
+  secret_key = "yyy"
 }
